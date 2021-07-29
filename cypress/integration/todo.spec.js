322d9bc3ff1563831732bcab815242738b70c113
Inga <inga.s6699@gmail.com>
2021-07-29 11:09:58 +0200

    feat: add cypress for e2e tests

---
+++ cypress/integration/todo.spec.js
@@ -0,0 +1,17 @@
+describe("todo App", () => {
+  it("should allow to enter text", () => {
+    cy.visit("/");
+    cy.get("[data-cy='input']")
+      .type("llearn cypress")
+      .should("have.value", "learn cypress");
+  });
+
+  it("should add li elements", () => {
+    cy.visit("/");
+    cy.get("[data-cy='input']").type("learn cypress");
+
+    cy.get("[data-cy='button']").click();
+
+    cy.get("[data-cy='list']").get("li").should("have.length", 1);
+  });
+});
